ee5eccf4c1d8f74e6a987f50cb4ea49fd579c294
jtimberman <joshua@opscode.com>
2013-04-29 20:03:53 -0600

    increment version for development

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "andrea.campi@zephirworks.com"
 license          "Apache 2.0"
 description      "Deploys and configures PHP-based applications"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          "1.1.1"
 
 depends "apache2"
 depends "php"
