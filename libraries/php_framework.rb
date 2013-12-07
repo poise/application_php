eb4625baca70fb0a680920c4930633886f83d988
Maks <github.maks3w@virtualplanets.net>
2013-12-07 11:11:31 +0100

    wrap composer call with deployment key
    Composer calls should be wrapped with the GIT wrapper for access from private repositories

--- libraries/php_framework.rb
+++ libraries/php_framework.rb
@@ -34,6 +34,7 @@ class ApplicationPhpCookbook
               to "#{new_resource.path}/shared/vendor"
             end
             execute "#{new_resource.composer_command} install -n -q #{new_resource.composer_options}" do
+              environment ({'GIT_SSH' => git_ssh_wrapper})
               cwd new_resource.release_path
               user new_resource.owner
             end
